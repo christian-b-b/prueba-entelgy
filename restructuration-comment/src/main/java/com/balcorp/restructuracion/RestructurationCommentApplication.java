5b5081d33975df79342ab3b4976ec6aadb68600c
christian-b-b <cbaldeon.github@gmail.com>
2021-03-23 18:01:54 -0500

    initializer

---
+++ restructuration-comment/src/main/java/com/balcorp/restructuracion/RestructurationCommentApplication.java
@@ -0,0 +1,13 @@
+package com.balcorp.restructuracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestructurationCommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestructurationCommentApplication.class, args);
+	}
+
+}
---
+++ restructuration-comment/src/test/java/com/balcorp/restructuracion/RestructurationCommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balcorp.restructuracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestructurationCommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
